935e2ef6a93b03fd8e207a381a3edb5ae2bbf78c
Fernando Romero <fer.rom.mu@gmail.com>
2022-03-10 00:22:38 -0300

    Nav responsive completo.

---
+++ script.js
@@ -0,0 +1,30 @@
+responsiveMenuButton = document.querySelector(".navfixed__icon");
+responsiveOpt = document.querySelectorAll(".navfixed__linkOpt");
+responsiveNav = document.querySelector(".navfixed__menu");
+about = document.querySelector(".proyects");
+
+function showMenu() {
+  responsiveNav.classList.remove('hidden');
+}
+
+function hideMenu() {
+  responsiveNav.classList.add('hidden');
+}
+
+responsiveMenuButton.addEventListener('click', () => showMenu());
+responsiveOpt.forEach(opt => {
+    opt.addEventListener('click', () => hideMenu());
+  }
+);
+
+window.onscroll = function() {
+  let position = about.getBoundingClientRect().y;
+  let scroll = document.documentElement.scrollTop;
+  if(window.matchMedia("(max-width: 555px)").matches) {
+    responsiveMenuButton.style = "";
+  }else if(scroll > position) {
+    responsiveMenuButton.style.transform = "scale(1)";
+  }else {
+    responsiveMenuButton.style.transform = "scale(0)";
+  }
+}
